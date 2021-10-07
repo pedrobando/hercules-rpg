c2e9f2dfa31338a3ee1f6807bdfc58d3a0dc958d
pedrobando <pedrobandorivera@gmail.com>
2021-10-06 19:19:07 -0500

    Copy the same function and create one per enemy.

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ function generateAttack(attackArray, damageArray){
     return attackArray[Math.floor(Math.random() * attackArray.length)], damageArray[Math.floor(Math.random() * damageArray.length)];
 }
 
+function hercules(attack, damage){
+    attack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
+    damage = generateAttack[20, 30, 20, 25, 15];
+    return attack, damage 
+}
+
+function nemeanLion(nAttack, nDamage){
+    nAttack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
+    nDamage = generateAttack[20, 30, 20, 25, 15];
+    return nAttack, nDamage 
+}
+
+function hydra(hAttack, hDamage){
+    hAttack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
+    hDamage = generateAttack[20, 30, 20, 25, 15];
+    return hAttack, hDamage 
+}
+
+function cerberus(cAttack, cDamage){
+    cAttack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
+    cDamage = generateAttack[20, 30, 20, 25, 15];
+    return cAttack, cDamage 
+}
+
