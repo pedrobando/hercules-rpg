c02fd3f32662c593f7179e834871d557eac116af
pedrobando <pedrobandorivera@gmail.com>
2021-10-06 21:41:33 -0500

    Create a function to defeat the first enemy. Take attacker and defender as parameter. Create a while loop that checks that both characters's health is not equal to zero. Inside of the while loop run the attack function passing the same parameters as the function you are working inside of. Console log the attacker's name, the attack's name and the defenders health. Create an if statment that checks if the defenders health is not zero. Call the same attack function with the parameters reversed. Console log the defender's name, attack's name, and attacker's health.

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 let hercules = {
 	health : 100, 
-    attacks : [{heavyAttack:25, name: "Sword"},{lightAttack:20, name: "Grapple"},{smallAttcak:5, name: "punch"}] 
+    attacks : [{heavyAttack:25, name: "Sword"},{lightAttack:20, name: "Grapple"},{smallAttcak:5, name: "punch"}],
+    name : "Hercules" 
     }
     
 let lion = {
 	health : 100, 
-    attacks : [{heavyAttack:25, name: "Crunch"},{lightAttack:20, name: "Tackle"},{smallAtack:5, name: "Roar" }] 
+    attacks : [{heavyAttack:25, name: "Crunch"},{lightAttack:20, name: "Tackle"},{smallAtack:5, name: "Roar" }],
+    name : "Nemean Lion"     
     }
 
 let hydra = {
     health : 100, 
-    attacks : [{heavyAttack:25, name: "Fire"},{lightAttack:20, name: "9 Head Tackle"},{smallAtack:5, name: "Spit Venom" }] 
+    attacks : [{heavyAttack:25, name: "Fire"},{lightAttack:20, name: "9 Head Tackle"},{smallAtack:5, name: "Spit Venom" }],
+    name: "Lernaean Hydra"
     }
 
 let dog = {
     health : 100, 
-    attacks : [{heavyAttack:25, name: "Bite"},{lightAttack:20, name: "Tackle"},{smallAtack:5, name: "Scratch" }] 
+    attacks : [{heavyAttack:25, name: "Bite"},{lightAttack:20, name: "Tackle"},{smallAtack:5, name: "Scratch" }],
+    name: "Cerberus" 
     }
 
 function attackIt(a, d){
-	d.health -= a.attacks[Math.floor(Math.random()* a.attacks.length)].0;
+	d.health -= a.attacks[0].heavyAttack;
   
 }
+
+
+//let test = dog.attacks[Math.floor(Math.random()* dog.attacks.length)];
+//console.log(test)
+
+function slayDragon(a, d){
+    while(a.health != 0 && d.health != 0){
+ 	    attackIt(a,d);
+         console.log(`${a.name} used ${a.attacks[0].name}, ${d.name} health is now ${d.health}`);
+         if(d.health != 0 && a.health != 0){
+            attackIt(d,a);
+             console.log(`${d.name} used ${d.attacks[0].name}, ${a.name} health is now ${a.health}`);
+        }
+    }
+    
+  
+  console.log(hercules.health, lion.health)
+ }
+
+ slayDragon(hercules, lion)
