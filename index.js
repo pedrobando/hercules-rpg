829f3509fbf61c188b41a1fad37b0e5d6e9019d0
pedrobando <pedrobandorivera@gmail.com>
2021-10-06 21:15:20 -0500

    Create variables for each character that would hold Health and Attacks objects. Assign 100 to health. Assign an array with attack names and damages as properties and values.

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 let hercules = {
 	health : 100, 
-    attacks : [{heavyAttack:25},{lightAttack:20},{punch:5}] 
+    attacks : [{heavyAttack:25, name: "Sword"},{lightAttack:20, name: "Grapple"},{smallAttcak:5, name: "punch"}] 
     }
     
 let lion = {
 	health : 100, 
-    attacks : [{heavyAttack:25, name: "Hammer"},{lightAttack:20},{punch:5}] 
+    attacks : [{heavyAttack:25, name: "Crunch"},{lightAttack:20, name: "Tackle"},{smallAtack:5, name: "Roar" }] 
     }
-    
-let fighters = [lion, hercules];
 
-function attackIt(a, d){
-	d.health -= a.attacks[0].heavyAttack;
-  
-}
-function slayDragon(a, d)
-{
- while(a.health > 0 && d.health > 0){
- 	attackIt(fighters[Math.floor(Math.random() * fighters.length)], fighters[Math.floor(Math.random() * fighters.length)]);
-  
-  console.log(hercules.health, lion.health)
- }
-}
-attackIt(hercules, lion);
-console.log(hercules.health, lion.health)
+let hydra = {
+    health : 100, 
+    attacks : [{heavyAttack:25, name: "Fire"},{lightAttack:20, name: "9 Head Tackle"},{smallAtack:5, name: "Spit Venom" }] 
+    }
 
-attackIt(lion, hercules);
-console.log(hercules.health, lion.health)
-
+let dog = {
+    health : 100, 
+    attacks : [{heavyAttack:25, name: "Bite"},{lightAttack:20, name: "Tackle"},{smallAtack:5, name: "Scratch" }] 
+    }
+ 
