4ab4a0c2c79d1d2ec09ddb81f30554765602b0a8
pedrobando <pedrobandorivera@gmail.com>
2021-10-06 20:36:19 -0500

    Erase everything.

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-"use strict" ;
-function generateAttack(attackArray, damageArray){
-    return attackArray[Math.floor(Math.random() * attackArray.length)], damageArray[Math.floor(Math.random() * damageArray.length)];
-}
+let hercules = {
+	health : 100, 
+    attacks : [{heavyAttack:25},{lightAttack:20},{punch:5}] 
+    }
+    
+let lion = {
+	health : 100, 
+    attacks : [{heavyAttack:25, name: "Hammer"},{lightAttack:20},{punch:5}] 
+    }
+    
+let fighters = [lion, hercules];
 
-function hercules(attack, damage){
-    attack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
-    damage = generateAttack[20, 30, 20, 25, 15];
-    return attack, damage 
+function attackIt(a, d){
+	d.health -= a.attacks[0].heavyAttack;
+  
 }
+function slayDragon(a, d)
+{
+ while(a.health > 0 && d.health > 0){
+ 	attackIt(fighters[Math.floor(Math.random() * fighters.length)], fighters[Math.floor(Math.random() * fighters.length)]);
+  
+  console.log(hercules.health, lion.health)
+ }
+}
+attackIt(hercules, lion);
+console.log(hercules.health, lion.health)
 
-function nemeanLion(nAttack, nDamage){
-    nAttack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
-    nDamage = generateAttack[20, 30, 20, 25, 15];
-    return nAttack, nDamage 
-}
-
-function hydra(hAttack, hDamage){
-    hAttack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
-    hDamage = generateAttack[20, 30, 20, 25, 15];
-    return hAttack, hDamage 
-}
-
-function cerberus(cAttack, cDamage){
-    cAttack = generateAttack["Heavy Attack", "Light Attack", "Heavy Sword Attack"];
-    cDamage = generateAttack[20, 30, 20, 25, 15];
-    return cAttack, cDamage 
-}
+attackIt(lion, hercules);
+console.log(hercules.health, lion.health)
 
