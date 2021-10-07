5c6fb7dee251189e2c28f34622ed47dcc67ef859
pedrobando <pedrobandorivera@gmail.com>
2021-10-06 22:02:23 -0500

    Add the set of alert that will describe whats going on using the attachker's and defender's name, their attacks, and health.

--- index.js
+++ index.js
@@ -30,15 +30,22 @@ function attackIt(a, d){
 
 //let test = dog.attacks[Math.floor(Math.random()* dog.attacks.length)];
 //console.log(test)
-
+alert("Hercules, the greatest of the Greek Heroes! You have been tasked by King Eurystheus to slay the vicious Nemean Lion, defeat the impossible nine-headed Lernaean Hydra, and capture the guard dog of the underworld—Cerberus. Will Hercules be successful?? Let's see!");
+alert("Ohh no! Hercules is in trouble! Hades heard the King's order and sent the vicious Nemean Lion to Hercules location.");
+alert("The Nemean Lion caught Hercules by surpriseee!!! Now their match begins....")
 function slayDragon(a, d){
     while(a.health != 0 && d.health != 0){
  	    attackIt(a,d);
+         alert(`${a.name} used ${a.attacks[0].name}, ${d.name} health is now ${d.health}!! Go go go ${a.name}`);
          console.log(`${a.name} used ${a.attacks[0].name}, ${d.name} health is now ${d.health}`);
          if(d.health != 0 && a.health != 0){
             attackIt(d,a);
+            alert(`${d.name} used ${d.attacks[0].name}, ${a.name} health is now ${a.health} Buuuuuu Bu Buuuu ${d.name}`);
              console.log(`${d.name} used ${d.attacks[0].name}, ${a.name} health is now ${a.health}`);
         }
+    }
+    if(d.health == 0){
+        alert(`${d.name} has been defeated by ${a.name} Hercules! Would Hades send the 9 headed Hydra? Or will they wait for Hercules to get there?`);
     }
     
   
